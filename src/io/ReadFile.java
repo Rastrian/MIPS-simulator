b1a9e96f77184220d75f146f5e2d48e30eed20a2
Luis Rondow <luiseduardorondow@gmail.com>
2020-03-27 16:18:18 -0300

    get io methods

---
+++ src/io/ReadFile.java
@@ -0,0 +1,26 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ReadFile
+ */
+public class ReadFile {
+
+    public static List<String> getCommands(String path) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+
+        List<String> commandsList = new ArrayList<String>();
+
+        while (bufferedReader.ready()) {
+            commandsList.add(bufferedReader.readLine());
+        }
+        bufferedReader.close();
+
+        return commandsList;
+    }
+}
---
+++ src/io/WriteFile.java
@@ -0,0 +1,18 @@
+package io;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * WriteFile
+ */
+public class WriteFile {
+
+    public static void writeBinary(String path, String line) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path));
+
+        bufferedWriter.append(line + "\n");
+        bufferedWriter.close();
+    }
+}
